75a0631a3dcc44225c50ed61fff55178be2402ba
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-04-27 21:36:31 +0300

    add icon for v1

--- prototype/v1/frontend/iceocean/src/layouts/default/AppBar.vue
+++ prototype/v1/frontend/iceocean/src/layouts/default/AppBar.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app-bar color="purple-darken-4">
+
+    <v-img
+    class="ml-15"
+    src="@/assets/logo_iceocean.svg"
+    max-height="80"
+    max-width="80"
+    contain
+    ></v-img>
     <v-app-bar-title>
-      <div class="ml-15 text-h4" >ICEocean</div>
+      <div class="text-h4" >ICEocean</div>
       <!-- <v-icon icon="mdi-circle-slice-4" /> -->
     </v-app-bar-title>
     <div class="mr-15">
