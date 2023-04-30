864f3ebecdbf200bc8508b552756e7122e3cad7c
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-04-30 14:07:39 +0300

    add func to create area for algorithm

--- prototype/v1/backend/tools/main.py
+++ prototype/v1/backend/tools/main.py
@@ -14,7 +14,9 @@ start_latitude = 68.91316126089126
 end_longitude = 64.89570320937713
 end_latitude = 74.67013839379428
 
-build(map_, width, length, start_longitude, start_latitude, end_longitude, end_latitude)
+area = build(map_, width, length, start_longitude, start_latitude, end_longitude, end_latitude)
 
-with open("check_map.json", "w") as file:
-    json.dump(map_, file)
+with open("area_check.txt", "w") as file:
+    for length in area:
+        file.write(str(length))
+        file.write("\n")
--- prototype/v1/backend/tools/route_building_area.py
+++ prototype/v1/backend/tools/route_building_area.py
@@ -6,7 +6,6 @@ def find_min_elem(map_, width, length, longitude, latitude):
         for y in range(width):
             for x in range(length):
                 if map_[y][x]["longitude"] >= 0 and map_[y][x]["latitude"] >= 0:
-                    print(map_[y][x])
                     return map_[y][x]
 
     elif longitude < 0 and latitude < 0:
@@ -49,13 +48,8 @@ def nearest(map_, width, length, longitude, latitude):
             for x in range(length):
 
                 if map_[y][x]["longitude"] >= 0 and map_[y][x]["latitude"] >= 0:
-                    # print(map_[y][x]["longitude"], map_[y][x]["latitude"])
-                    # with open("check_if.txt", "w") as file:
-                    #     file.write(str(map_[y][x]))
-                    #     file.write("\n")
                     if check_diff(map_[y][x], longitude, latitude, difference_long, difference_lat):
                         min_elem, difference_long, difference_lat = define_diff(map_[y][x], longitude, latitude)
-                        print(min_elem, difference_long, difference_lat)
 
     elif longitude < 0 and latitude < 0:
         for y in range(width):
@@ -83,9 +77,65 @@ def nearest(map_, width, length, longitude, latitude):
     return y, x
 
 
+def get_length_area_border(y_start, x_start, y_end, x_end):
+    if y_start > y_end:
+        area_width = y_start - y_end + 1
+    elif y_start < y_end:
+        area_width = y_end - y_start + 1
+
+    if x_start > x_end:
+        area_length = x_start - x_end + 1
+    elif x_start < x_end:
+        area_length = x_end - x_start + 1
+
+    return area_width, area_length
+
+
+def get_vertex_area_border(y_start, x_start, y_end, x_end):
+    print(y_start, x_start, y_end, x_end)
+    if y_start > y_end:
+        y_vertex_top = y_end
+        y_vertex_bottom = y_start
+
+    elif y_start < y_end:
+        y_vertex_top = y_start
+        y_vertex_bottom = y_end
+
+    if x_start > x_end:
+        x_vertex_right = x_start
+        x_vertex_left = x_end
+
+    elif x_start < x_end:
+        x_vertex_right = x_end
+        x_vertex_left = x_start
+
+    return y_vertex_top, y_vertex_bottom, x_vertex_right, x_vertex_left
+
+
 def build(map_, width, length, start_longitude, start_latitude, end_longitude, end_latitude):
     y_start, x_start = nearest(map_, width, length, start_longitude, start_latitude)
     y_end, x_end = nearest(map_, width, length, end_longitude, end_latitude)
 
+    print(f"старт {y_start}, {x_start}")
+    print(f"финиш {y_end}, {x_end}")
     map_[y_start][x_start]["start"] = True
     map_[y_end][x_end]["end"] = True
+
+    area_width, area_length = get_length_area_border(y_start, x_start, y_end, x_end)
+    y_vertex_top, y_vertex_bottom, x_vertex_right, x_vertex_left \
+        = get_vertex_area_border(y_start, x_start, y_end, x_end)
+
+    area = [['.' for x in range(area_length)] for y in range(area_width)]
+
+    print(f"y_vertex_top: {y_vertex_top}")
+    print(f"y_vertex_bottom: {y_vertex_bottom}")
+    print(f"x_vertex_right: {x_vertex_right}")
+    print(f"x_vertex_left: {x_vertex_left}")
+
+    for y in range(width):
+        for x in range(length):
+            if y_vertex_top <= y <= y_vertex_bottom:
+                if x_vertex_left <= x <= x_vertex_right:
+                    area[y - y_vertex_top][x - x_vertex_left] = map_[y][x]
+
+    return area
