7dc95cf5b8f4c9d9b64f0ffd2b391820b2cf1193
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-04-30 15:40:51 +0300

    add create graph from area

---
+++ prototype/v1/backend/tools/create_graph.py
@@ -0,0 +1,118 @@
+def create(area):
+    width = len(area)
+    length = len(area[0])
+
+    graph = {}
+
+    for y in range(width):
+        for x in range(length):
+            if y == 0 and x == 0:
+                area[y][x + 1]['weight'] = 1
+                area[y + 1][x]['weight'] = 1
+                area[y + 1][x + 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y][x + 1]['longitude']}|{area[y][x + 1]['latitude']}": area[y][x + 1]['weight'],
+                    f"{area[y + 1][x]['longitude']}|{area[y + 1][x]['latitude']}": area[y + 1][x]['weight'],
+                    f"{area[y + 1][x + 1]['longitude']}|{area[y + 1][x + 1]['latitude']}": area[y + 1][x + 1]
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            elif y == 0 and x == length - 1:
+                area[y][x - 1]['weight'] = 1
+                area[y + 1][x]['weight'] = 1
+                area[y + 1][x - 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y][x - 1]['longitude']}|{area[y][x - 1]['latitude']}":  area[y][x - 1]['weight'],
+                    f"{area[y + 1][x]['longitude']}|{area[y + 1][x]['latitude']}": area[y + 1][x]['weight'],
+                    f"{area[y + 1][x - 1]['longitude']}|{area[y + 1][x - 1]['latitude']}": area[y + 1][x - 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            elif y == width - 1 and x == 0:
+                area[y - 1][x]['weight'] = 1
+                area[y][x + 1]['weight'] = 1
+                area[y - 1][x + 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y - 1][x]['longitude']}|{area[y - 1][x]['latitude']}": area[y - 1][x]['weight'],
+                    f"{area[y][x + 1]['longitude']}|{area[y][x + 1]['latitude']}": area[y][x + 1]['weight'],
+                    f"{area[y - 1][x + 1]['longitude']}|{area[y - 1][x + 1]['latitude']}": area[y - 1][x + 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            elif y == width - 1 and x == length - 1:
+                area[y - 1][x]['weight'] = 1
+                area[y][x - 1]['weight'] = 1
+                area[y - 1][x - 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y - 1][x]['longitude']}|{area[y - 1][x]['latitude']}": area[y - 1][x]['weight'],
+                    f"{area[y][x - 1]['longitude']}|{area[y][x - 1]['latitude']}": area[y][x - 1]['weight'],
+                    f"{area[y - 1][x - 1]['longitude']}|{area[y - 1][x - 1]['latitude']}": area[y - 1][x - 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            elif y == 0:
+                area[y][x - 1]['weight'] = 1
+                area[y][x + 1]['weight'] = 1
+                area[y + 1][x]['weight'] = 1
+                area[y + 1][x - 1]['weight'] = 1.41
+                area[y + 1][x + 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y][x - 1]['longitude']}|{area[y][x - 1]['latitude']}": area[y][x - 1]['weight'],
+                    f"{area[y][x + 1]['longitude']}|{area[y][x + 1]['latitude']}": area[y][x + 1]['weight'],
+                    f"{area[y + 1][x]['longitude']}|{area[y + 1][x]['latitude']}": area[y + 1][x]['weight'],
+                    f"{area[y + 1][x - 1]['longitude']}|{area[y + 1][x - 1]['latitude']}": area[y + 1][x - 1]['weight'],
+                    f"{area[y + 1][x + 1]['longitude']}|{area[y + 1][x + 1]['latitude']}": area[y + 1][x + 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            elif y == width - 1:
+                area[y][x - 1]['weight'] = 1
+                area[y][x + 1]['weight'] = 1
+                area[y - 1][x]['weight'] = 1
+                area[y - 1][x - 1]['weight'] = 1.41
+                area[y - 1][x + 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y][x - 1]['longitude']}|{area[y][x - 1]['latitude']}": area[y][x - 1]['weight'],
+                    f"{area[y][x + 1]['longitude']}|{area[y][x + 1]['latitude']}": area[y][x + 1]['weight'],
+                    f"{area[y - 1][x]['longitude']}|{area[y - 1][x]['latitude']}": area[y - 1][x]['weight'],
+                    f"{area[y - 1][x - 1]['longitude']}|{area[y - 1][x - 1]['latitude']}": area[y - 1][x - 1]['weight'],
+                    f"{area[y - 1][x + 1]['longitude']}|{area[y - 1][x + 1]['latitude']}": area[y - 1][x + 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+            else:
+                area[y][x - 1]['weight'] = 1
+                area[y][x + 1]['weight'] = 1
+                area[y - 1][x]['weight'] = 1
+                area[y + 1][x]['weight'] = 1
+                area[y - 1][x - 1]['weight'] = 1.41
+                area[y - 1][x + 1]['weight'] = 1.41
+                area[y + 1][x + 1]['weight'] = 1.41
+                area[y + 1][x - 1]['weight'] = 1.41
+
+                nodes = {
+                    f"{area[y][x - 1]['longitude']}|{area[y][x - 1]['latitude']}": area[y][x - 1]['weight'],
+                    f"{area[y][x + 1]['longitude']}|{area[y][x + 1]['latitude']}": area[y][x + 1]['weight'],
+                    f"{area[y - 1][x]['longitude']}|{area[y - 1][x]['latitude']}": area[y - 1][x]['weight'],
+                    f"{area[y + 1][x]['longitude']}|{area[y + 1][x]['latitude']}": area[y + 1][x]['weight'],
+                    f"{area[y - 1][x - 1]['longitude']}|{area[y - 1][x - 1]['latitude']}": area[y - 1][x - 1]['weight'],
+                    f"{area[y - 1][x + 1]['longitude']}|{area[y - 1][x + 1]['latitude']}": area[y - 1][x + 1]['weight'],
+                    f"{area[y + 1][x + 1]['longitude']}|{area[y + 1][x + 1]['latitude']}": area[y + 1][x + 1]['weight'],
+                    f"{area[y + 1][x - 1]['longitude']}|{area[y + 1][x - 1]['latitude']}": area[y + 1][x - 1]['weight']
+                }
+
+                graph[f"{area[y][x]['longitude']}|{area[y][x]['latitude']}"] = nodes
+
+    return graph
--- prototype/v1/backend/tools/create_node.py
+++ prototype/v1/backend/tools/create_node.py
@@ -4,5 +4,6 @@ def create(longitude, latitude, start, end, type_of_ice):
         "latitude": latitude,
         "start": start,
         "end": end,
-        "type_of_ice": type_of_ice
+        "type_of_ice": type_of_ice,
+        "weight": 0
     }
